fc9666b508af21e61642d93d18cb611ae87014ae
Jacob Pascua <pascuajakecreed@gmail.com>
2023-03-23 18:43:51 -0400

    db.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,9 @@
 import * as functions from "firebase-functions";
+import express, {Application} from "express";
+import cors from "cors";
 
-// // Start writing functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app:Application = express();
+app.use(cors());
+app.use(express.json());
+
+export const api = functions.https.onRequest(app);
