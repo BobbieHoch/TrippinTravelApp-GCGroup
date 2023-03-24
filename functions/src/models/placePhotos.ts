47a32d747f45790d000d7a0fe6cb7f7c8331186b
BobbieHoch <rwhochstadt@gmail.com>
2023-03-23 21:54:13 -0400

    frontendmodelitinerary

---
+++ functions/src/models/placePhotos.ts
@@ -0,0 +1,19 @@
+
+
+
+
+export interface GooglePlacesPhotosAPIResponse {
+    status: string;
+    html_attributions: string[];
+    photo_reference: string;
+    height: number;
+    width: number;
+  }
+  
+  export interface Photo {
+    reference: string;
+    height: number;
+    width: number;
+    getUrl: (maxWidth?: number, maxHeight?: number) => Promise<string | undefined>;
+  }
+  
