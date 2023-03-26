82bbf841bb7b7d399e9ff075fbc73543005a24d6
Jacob Pascua <pascuajakecreed@gmail.com>
2023-03-26 01:55:21 -0400

    services and routes to the google api requests working via postman

--- functions/src/services/nearbySearchService.ts
+++ functions/src/services/nearbySearchService.ts
@@ -3,16 +3,16 @@ import { NearbySearch } from "../models/nearbySearch";
 
 const key = "AIzaSyADw6kne2LUqaF8G-njq1U66rgpNkOgM7c";
 
-
 export function getNearbySearch(
-  lat: Number,
-  lng: Number,
-  radius: Number
+  lat: number,
+  lng: number,
+  radius: number
 ): Promise<NearbySearch> {
+  const location = `${lat},${lng}`;
   return axios
     .get<NearbySearch>(
       "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-      { params: { lat, lng, radius, key } }
+      { params: { location, radius, key } }
     )
     .then((response) => response.data);
 }
--- functions/src/services/placeDetailsService.ts
+++ functions/src/services/placeDetailsService.ts
@@ -1,13 +1,12 @@
 import { PlaceDetails } from "../models/placeDetails";
-import googconfig from "../config/config";
 import axios from "axios";
 
-const key = googconfig;
+const key = "AIzaSyADw6kne2LUqaF8G-njq1U66rgpNkOgM7c";
 
-export function getTextSearch(place_id: Number): Promise<PlaceDetails> {
+export function getDetails(place_id: String): Promise<PlaceDetails> {
   return axios
     .get<PlaceDetails>(
-      "https://maps.googleapis.commaps/api/place/details/json",
+      "https://maps.googleapis.com/maps/api/place/details/json",
       {
         params: { place_id, key },
       }
