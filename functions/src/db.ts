8938c8244742682e595bd0d66ea85fe5e6db54f7
BobbieHoch <rwhochstadt@gmail.com>
2023-03-28 20:08:59 -0400

    removedduplicatenearbySeachServices

--- functions/src/db.ts
+++ functions/src/db.ts
@@ -1,7 +1,9 @@
+//connects to database
 import * as functions from "firebase-functions";
+//how we connect to Mongo
 import { MongoClient } from "mongodb";
 
-
+//grabs from firebase the connections string that is secure
 const uri: string = functions.config().mongodb.uri;
 if (!uri) {
   console.error("ERROR: Missing environment variable mongodb.uri.");
