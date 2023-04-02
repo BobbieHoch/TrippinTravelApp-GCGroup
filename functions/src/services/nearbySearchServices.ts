32b369730c2f8a42dcbda6f984a7bb164d7ae2d8
Martin Wisor <martinwisor@Martins-iMac.lan>
2023-04-01 21:19:52 -0400

    Nearby Search Services fix

--- functions/src/services/nearbySearchServices.ts
+++ functions/src/services/nearbySearchServices.ts
@@ -6,13 +6,14 @@ const key = "AIzaSyADw6kne2LUqaF8G-njq1U66rgpNkOgM7c";
 export function getNearbySearch(
   lat: Number,
   lng: Number,
-  radius: Number
+  radius: Number,
+  type: string
 ): Promise<NearbySearch> {
   const location = `${lat},${lng}`
   return axios
     .get<NearbySearch>(
       "https://maps.googleapis.com/maps/api/place/nearbysearch/json",
-      { params: { location, radius, key } }
+      { params: { location, radius, type, key } }
     )
     .then((response) => response.data);
 }
